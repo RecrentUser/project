e36484f62d7702f023e0f6f2d974a03838cba469
Nikita <semin.nikt@yandex.ru>
2022-06-05 17:56:14 +0300

    Условия

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+"use strict";
 
-const   a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', ''),
-        c = prompt('Один из последних просмотренных фильмов?', ''),
-        d = ('На сколько оцените его?', '');
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const num = 50;
 
-console.log(personalMovieDB);
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+
+const num = '50';
+// (num == 50) ? console.log('Ok!') : console.log('Error');
+switch (num) {
+    case '49':
+        console.log('Неверно');
+        break;
+    case '100':
+        console.log('Неверно');
+        break;
+    case '50':
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+}
