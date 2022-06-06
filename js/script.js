76c82eca7894b2be076a893bedb7ad35e6c6d5b8
Nikita <semin.nikt@yandex.ru>
2022-06-06 21:01:09 +0300

    task №1

--- js/script.js
+++ js/script.js
@@ -1,42 +1,21 @@
 "use strict";
 
-if (4 == 9) {
-    console.log('Ok!');
-} else {
-    console.log('Error');
-}
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const num = 50;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Много');
-// } else {
-//     console.log('Ok!');
-// }
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-// const num = '50';
-// // (num == 50) ? console.log('Ok!') : console.log('Error');
-// switch (num) {
-//     case '49':
-//         console.log('Неверно');
-//         break;
-//     case '100':
-//         console.log('Неверно');
-//         break;
-//     case '50':
-//         console.log('В точку!');
-//         break;
-//     default:
-//         console.log('Не в этот раз!');
-//         break;
-// }
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-const Name1 = 'Nikita';
-const Name2 = 'Alena';
-if (Name2 == 'Alena') {
-    console.log('Yes!');
-} else {
-    console.log('No!');
-}
+console.log(personalMovieDB);
